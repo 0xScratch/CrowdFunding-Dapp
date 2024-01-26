2de53da28d10e0a4ba88383179972086d97c192a
Aryan Malik <aryanhunny98@gmail.com>
2024-01-26 13:31:42 +0530

    Wallet Connection

--- script/Contract.s.sol
+++ script/Contract.s.sol
@@ -28,4 +28,4 @@ contract ContractScript is Script {
 
 // Test Network (get private_key and rpc_url from that test network and store it down in variables)
 
-// command - forge script script/Contract.s.sol:ContractScript --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast 
+// command - forge script script/Contract.s.sol:ContractScript --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast
